7ce8576660dc5e5bd200567894edb18c77827f09
sanjeev gurung <gurungsanjeev91@gmail.com>
2025-08-17 20:59:58 +0545

    added context file

---
+++ MERN_CHAT_APP/Client/src/Context/AuthProvider.jsx
@@ -0,0 +1,16 @@
+import React, { createContext } from 'react'
+
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AuthProvider
